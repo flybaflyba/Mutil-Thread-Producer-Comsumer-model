ee90c0cb35387ad4d1804909f72a86a869358f11
Litian Zhang <litian_zhang17@163.com>
2020-10-23 07:09:45 +0000

    Implement semaphore, only one thread can access sum now, sum is back to the right number

--- threads.c
+++ threads.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h> // allows more than 1 threads to access, we dont do this here though
 
 void *do_math(void * arg); //void * a pointer that can point to anything
 
 int sum;
+sem_t sum_lock;
 
 int main(int argc, char **argv) {
     
@@ -22,6 +24,12 @@ int main(int argc, char **argv) {
     num_threads = atoi(argv[1]);
     
     threads = (pthread_t*) calloc(num_threads, sizeof(pthread_t));
+    
+    if (sem_init(&sum_lock, 0, 1) < 0) { // 0: not share with other process, 1: one thread to go through at one time
+        perror("Error initizing sum_lock");
+        return 0;
+    }
+    
     
     printf("Launching %d threads\n", num_threads);
 
@@ -51,9 +59,11 @@ void *do_math(void *arg) {
     int temp;
     printf("Thread %d launched ...\n", t_id);
     for (int n=0; n<100; n++) {
-        temp = sum;
+        sem_wait(&sum_lock); // sum_lock is decremented 1 to 0 now, no thread can get in
+        temp = sum; // critical section, 4 lines 
         usleep(5);
         temp++;
-        sum = temp;
+        sum = temp; 
+        sem_post(&sum_lock); // sum_lock is incremented 1 to 1 now, one threaad can get in 
     }
 }
